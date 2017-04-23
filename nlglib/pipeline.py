f7f8a320e3477d518e7ad45d7f18c50be5ee95b5
Roman Kutlak <roman@kutlak.net>
2017-04-23 12:46:32 +0100

    Added flask-like context.

--- nlglib/pipeline.py
+++ nlglib/pipeline.py
@@ -9,20 +9,62 @@ from werkzeug.datastructures import ImmutableDict
 from . import macroplanning, lexicalisation, aggregation, reg, realisation, format
 
 from .config import Config, ConfigAttribute
+from .ctx import PipelineContext
+from .globals import _pipeline_ctx_stack
+from .signals import pipeline_context_tearing_down
 from .utils import PackageBoundObject, locked_cached_property, find_package
+from .lexicon import en
+
+# a singleton sentinel value for parameter defaults
+_sentinel = object()
 
 _logger_lock = Lock()
 
 
+def get_debug_flag(default=None):
+    val = os.environ.get('NLGLIB_DEBUG')
+    if not val:
+        return default
+    return val.lower() not in ('0', 'false', 'no')
+
+
 class Pipeline(PackageBoundObject):
-    debug = ConfigAttribute('DEBUG')
+    #: The name of the logger to use.  By default the logger name is the
+    #: package name passed to the constructor.
+    #:
+    #: .. versionadded:: 0.4
     logger_name = ConfigAttribute('LOGGER_NAME')
+
+    #: The debug flag.  Set this to ``True`` to enable debugging of the
+    #: application.  In debug mode the debugger will kick in when an unhandled
+    #: exception occurs and the integrated server will automatically reload
+    #: the application if changes in the code are detected.
+    #:
+    #: This attribute can also be configured from the config with the ``DEBUG``
+    #: configuration key.  Defaults to ``False``.
+    debug = ConfigAttribute('DEBUG')
+
+    #: The testing flag.  Set this to ``True`` to enable the test mode of
+    #: Flask extensions (and in the future probably also Flask itself).
+    #: For example this might activate unittest helpers that have an
+    #: additional runtime cost which should not be enabled by default.
+    #:
+    #: If this is enabled and PROPAGATE_EXCEPTIONS is not changed from the
+    #: default it's implicitly enabled.
+    #:
+    #: This attribute can also be configured from the config with the
+    #: ``TESTING`` configuration key.  Defaults to ``False``.
+    testing = ConfigAttribute('TESTING')
 
     # noinspection PyTypeChecker
     #: Default configuration parameters.
     default_config = ImmutableDict({
-        'DEBUG': False,
+        'DEBUG': get_debug_flag(default=False),
+        'TESTING': False,
+        'PROPAGATE_EXCEPTIONS': None,
+        'PRESERVE_CONTEXT_ON_EXCEPTION': None,
         'LOGGER_NAME': None,
+        'LEXICON': en.Lexicon,
         'CONTENT_PREPROCESSING': macroplanning.preprocess_content,
         'CONTENT_SELECTION': macroplanning.select_content,
         'CONTENT_AGGREGATION': macroplanning.aggregate_content,
@@ -36,7 +78,6 @@ class Pipeline(PackageBoundObject):
     })
 
     _logger = None
-    context = None
 
     def __init__(self, import_name, instance_path=None,
                  instance_relative_config=False, root_path=None):
@@ -67,25 +108,29 @@ class Pipeline(PackageBoundObject):
             'FORMATTING',
         ]
 
+        #: A list of functions that are called when the pipeline context
+        #: is destroyed.
+        self.teardown_pipeline_context_funcs = []
+        self.lexicon = self.config['LEXICON']()
+
     def process(self, data, **kwargs):
-        kwargs['_pipeline'] = self
-        if not self.context:
-            self.context = PipelineContext(self)
-        kwargs_ = self.context.kwargs.copy()
-        kwargs_.update(kwargs)
+        context = _pipeline_ctx_stack.top
+        if not context:
+            with self.pipeline_context(kwargs):
+                return self.process(data, **kwargs)
         rv = data
         # TODO: add pre/post signals
         for name in self.stages:
             self.logger.debug('running stage {}.'.format(name))
-            func = self.context.stages.get(name)
+            func = context.stages.get(name)
             if func:
-                rv = func(rv, **kwargs_)
+                rv = func(rv, **kwargs)
             else:
                 self.logger.debug('    --> skipping stage {}.'.format(name))
             self.logger.debug('finished stage {}.'.format(name))
         return rv
 
-    def make_config(self, instance_relative=False):
+    def make_config(self, instance_relative=False, mappings=None):
         """Used to create the config attribute by the Flask constructor.
         The `instance_relative` parameter is passed in from the constructor
         of Flask (there named `instance_relative_config`) and indicates if
@@ -93,6 +138,8 @@ class Pipeline(PackageBoundObject):
         of the application.
 
         """
+        if mappings:
+            return Config(self.root_path, mappings)
         root_path = self.root_path
         if instance_relative:
             root_path = self.instance_path
@@ -133,6 +180,30 @@ class Pipeline(PackageBoundObject):
             self._logger = rv = logging.getLogger(self.logger_name)
             return rv
 
+    @property
+    def propagate_exceptions(self):
+        """Returns the value of the ``PROPAGATE_EXCEPTIONS`` configuration
+        value in case it's set, otherwise a sensible default is returned.
+
+        .. versionadded:: 0.7
+        """
+        rv = self.config['PROPAGATE_EXCEPTIONS']
+        if rv is not None:
+            return rv
+        return self.testing or self.debug
+
+    @property
+    def preserve_context_on_exception(self):
+        """Returns the value of the ``PRESERVE_CONTEXT_ON_EXCEPTION``
+        configuration value in case it's set, otherwise a sensible default
+        is returned.
+        
+        """
+        rv = self.config['PRESERVE_CONTEXT_ON_EXCEPTION']
+        if rv is not None:
+            return rv
+        return self.debug
+
     def auto_find_instance_path(self):
         """Tries to locate the instance path if it was not provided to the
         constructor of the application class.  It will basically calculate
@@ -145,31 +216,67 @@ class Pipeline(PackageBoundObject):
             return os.path.join(package_path, 'instance')
         return os.path.join(prefix, 'var', self.name + '-instance')
 
+    def do_teardown_pipeline_context(self, exc=_sentinel):
+        """Called when a pipeline context is popped. """
+        if exc is _sentinel:
+            exc = sys.exc_info()[1]
+        for func in reversed(self.teardown_pipeline_context_funcs):
+            func(exc)
+        pipeline_context_tearing_down.send(self, exc=exc)
 
-class PipelineContext(ContextDecorator):
-    def __init__(self, pipeline=None, **kwargs):
-        self.pipeline = pipeline
-        self.kwargs = dict(kwargs)
-        self.stages = dict([
-            ('CONTENT_PREPROCESSING', self.pipeline.config.get('CONTENT_PREPROCESSING')),
-            ('CONTENT_SELECTION', self.pipeline.config.get('CONTENT_SELECTION')),
-            ('CONTENT_AGGREGATION', self.pipeline.config.get('CONTENT_AGGREGATION')),
-            ('CONTENT_STRUCTURING', self.pipeline.config.get('CONTENT_STRUCTURING')),
-            ('LEXICALISATION', self.pipeline.config.get('LEXICALISATION')),
-            ('AGGREGATION', self.pipeline.config.get('AGGREGATION')),
-            ('PRONOMINALISATION', self.pipeline.config.get('PRONOMINALISATION')),
-            ('REFERRING', self.pipeline.config.get('REFERRING')),
-            ('REALISATION', self.pipeline.config.get('REALISATION')),
-            ('FORMATTING', self.pipeline.config.get('FORMATTING')),
-        ])
+    def pipeline_context(self, config=None):
+        """Creates a :class:`~nlglib.ctx.PipelineContext` from the optional
+        config and binds it to the current context. For as long as the pipeline is bound
+        to the current context the :data:`nlglib.current_pipeline` points to that
+        pipeline. This must be used in
+        combination with the ``with`` statement because the pipeline is only bound
+        to the current context for the duration of the ``with`` block.
 
-    def __enter__(self):
-        self.original_context = self.pipeline.context
-        self.pipeline.context = self
-        return self
+        Example usage::
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.pipeline.context = self.original_context
-        self.original_context = None
-        del self.__dict__['pipeline']
-        return False
+            with pipe.pipeline_context():
+                do_something_with(pipe)
+
+        The object returned can also be used without the ``with`` statement.
+        The example above is doing exactly the same as this code::
+
+            ctx = pipe.pipeline_context()
+            ctx.push()
+            try:
+                do_something_with(pipeline)
+            finally:
+                ctx.pop()
+
+        :param config: a config dict
+        """
+        return PipelineContext(self, config)
+
+
+#
+# class PipelineContext(ContextDecorator):
+#     def __init__(self, pipeline=None, **kwargs):
+#         self.pipeline = pipeline
+#         self.kwargs = dict(kwargs)
+#         self.stages = dict([
+#             ('CONTENT_PREPROCESSING', self.pipeline.config.get('CONTENT_PREPROCESSING')),
+#             ('CONTENT_SELECTION', self.pipeline.config.get('CONTENT_SELECTION')),
+#             ('CONTENT_AGGREGATION', self.pipeline.config.get('CONTENT_AGGREGATION')),
+#             ('CONTENT_STRUCTURING', self.pipeline.config.get('CONTENT_STRUCTURING')),
+#             ('LEXICALISATION', self.pipeline.config.get('LEXICALISATION')),
+#             ('AGGREGATION', self.pipeline.config.get('AGGREGATION')),
+#             ('PRONOMINALISATION', self.pipeline.config.get('PRONOMINALISATION')),
+#             ('REFERRING', self.pipeline.config.get('REFERRING')),
+#             ('REALISATION', self.pipeline.config.get('REALISATION')),
+#             ('FORMATTING', self.pipeline.config.get('FORMATTING')),
+#         ])
+#
+#     def __enter__(self):
+#         self.original_context = self.pipeline.context
+#         self.pipeline.context = self
+#         return self
+#
+#     def __exit__(self, exc_type, exc_val, exc_tb):
+#         self.pipeline.context = self.original_context
+#         self.original_context = None
+#         del self.__dict__['pipeline']
+#         return False
