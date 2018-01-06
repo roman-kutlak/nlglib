6ccb9aece785d863db270e7c2c4aa0f00f476887
Roman Kutlak <roman@kutlak.net>
2018-01-06 23:01:20 +0000

    Starting on backcast.

---
+++ examples/backcast/__init__.py
@@ -0,0 +1,10 @@
+"""
+This package is an example of a simple NLG system.
+
+The system produces weather "backcast" (as opposed to
+forecast) from historic weather data. The data used
+in this example is from the UK MetOffice:
+https://www.metoffice.gov.uk/climate/uk/summaries/datasets
+
+
+"""
